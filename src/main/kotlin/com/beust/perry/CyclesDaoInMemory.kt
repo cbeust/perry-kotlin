5d577a867bc87851416831927a5f5977e61398bd
Cedric Beust <cedric@beust.com>
2019-03-05 19:15:04 -0800

    InMemory DAO.

---
+++ src/main/kotlin/com/beust/perry/CyclesDaoInMemory.kt
@@ -0,0 +1,12 @@
+package com.beust.perry
+
+class CyclesDaoInMemory: CyclesDao {
+    private val cycles = listOf(
+            Cycle(1, "Die dritte machte", "The third power", "third-power", 0, 50),
+            Cycle(2, "Atlan und Arkon", "Atlan and Arkonis", "atlan", 50, 99)
+    )
+
+    override fun allCycles() = CyclesDao.CyclesResponse(cycles)
+
+    override fun findCycle(n: Int) = if (n > 0 && n < cycles.size) cycles[n - 1] else null
+}
