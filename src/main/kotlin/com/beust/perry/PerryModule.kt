b454dd4a2d0ea8271174382049c19e7140f1ac43
Cedric Beust <cedric@beust.com>
2019-03-07 12:54:48 -0800

    Database credentials.

--- src/main/kotlin/com/beust/perry/PerryModule.kt
+++ src/main/kotlin/com/beust/perry/PerryModule.kt
@@ -3,6 +3,7 @@ package com.beust.perry
 import com.google.inject.Binder
 import com.google.inject.Module
 import com.google.inject.Singleton
+import java.net.URI
 import kotlin.to as _
 
 class PerryModule : Module {
@@ -14,14 +15,36 @@ class PerryModule : Module {
 
         // DAO's
         fun initJdbc(className: String) {
-            val user = localProperties.getOrNull(LocalProperty.DATABASE_USER)
-            val password = localProperties.getOrNull(LocalProperty.DATABASE_PASSWORD)
-            val url = localProperties.getRequired(LocalProperty.DATABASE_URL)
+            // postgres://{user}:{password}@{hostname}:{port}/{database-name}
+            val envDbUrl = System.getenv("DATABASE_URL")
+            val (dbUrl, user, password) =
+                    if (envDbUrl != null) {
+                        // Heroku, extract username and password from DATABASE_URL
+                        URI(envDbUrl).let { dbUri ->
+                            dbUri.userInfo.split(":").let { split ->
+                                val username = split[0]
+                                val password = split[1]
+                                val jdbcUrl = System.getenv("JDBC_DATABASE_URL")
+                                Triple(jdbcUrl, username, password)
+                            }
+                        }
+                    } else {
+                        // Local
+                        val envUsername = localProperties.get(LocalProperty.DATABASE_USER)
+                        val envPassword = localProperties.get(LocalProperty.DATABASE_PASSWORD)
+                        val database = localProperties.get(LocalProperty.DATABASE)
+                        val dbUrl = "jdbc:$database:demo"
+                        Triple(dbUrl, envUsername, envPassword)
+                    }
+
+//        val user = localProperties.getOrNull(LocalProperty.DATABASE_USER)
+//        val password = localProperties.getOrNull(LocalProperty.DATABASE_PASSWORD)
+//        val url = localProperties.getRequired(LocalProperty.DATABASE_URL)
             if (user != null && password != null) {
-                org.jetbrains.exposed.sql.Database.connect(url, driver = className,
+                org.jetbrains.exposed.sql.Database.connect(dbUrl, driver = className,
                         user = user, password = password)
             } else {
-                org.jetbrains.exposed.sql.Database.connect(url, driver = className)
+                org.jetbrains.exposed.sql.Database.connect(dbUrl, driver = className)
             }
         }
 
