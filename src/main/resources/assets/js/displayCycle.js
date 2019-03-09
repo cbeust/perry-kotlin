76dfaa5ea74ea2f3bda48ac09f8b2b809d3d9c95
Cedric Beust <cedric@beust.com>
2019-03-09 13:27:27 -0800

    Query parameters.

--- src/main/resources/assets/js/displayCycle.js
+++ src/main/resources/assets/js/displayCycle.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     computed: {
         cycle: function() {
-            var number = 3;
+            var urlParams = new URLSearchParams(window.location.search);
+            var number = urlParams.get("number");
             var cycle = JSON.parse(httpGet('/api/cycles/' + number));
             return cycle;
         }
