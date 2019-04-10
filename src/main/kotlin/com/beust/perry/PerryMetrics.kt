2f3550f1c1f21ba0ed542a2ceec204fc0abaee00
Cedric Beust <cedric@beust.com>
2019-04-09 22:26:07 -0700

    Days.

--- src/main/kotlin/com/beust/perry/PerryMetrics.kt
+++ src/main/kotlin/com/beust/perry/PerryMetrics.kt
@@ -4,6 +4,8 @@ import com.codahale.metrics.Gauge
 import com.google.inject.Inject
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Duration
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class CoverCountMetric @Inject constructor(private val coversDao: CoversDao): Gauge<Int> {
@@ -27,7 +29,8 @@ class CoverCacheMetric(val start: LocalDateTime): Gauge<String> {
     private var misses = 0
 
     override fun getValue(): String {
-        return "Hits/Misses: $hits/$misses, since ${Dates.formatDate(start)}"
+        val days = Duration.between(LocalDate.now().atStartOfDay(), start.toLocalDate().atStartOfDay()).toDays()
+        return "Hits/Misses: $hits/$misses, $days days ago"
     }
 
     fun addHit() = hits++
