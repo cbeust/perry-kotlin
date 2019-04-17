8503b8ff3ac96a66273f7d13c509c585181f1f31
Cedric Beust <cedric@beust.com>
2019-04-17 13:34:19 -0700

    Status for exception.

--- src/main/kotlin/com/beust/perry/Exceptions.kt
+++ src/main/kotlin/com/beust/perry/Exceptions.kt
@@ -1,5 +1,6 @@
 package com.beust.perry
 
 import javax.ws.rs.WebApplicationException
+import javax.ws.rs.core.Response
 
-class UserNotFoundException(message: String): WebApplicationException(message)
+class UserNotFoundException(message: String): WebApplicationException(message, Response.Status.NOT_FOUND)
