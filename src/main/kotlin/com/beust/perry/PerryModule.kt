24706a62dcf56fa4ceac6b98318c0b35b07431ca
Cedric Beust <cedric@beust.com>
2019-11-05 19:04:24 -0800

    Refactoring.

--- src/main/kotlin/com/beust/perry/PerryModule.kt
+++ src/main/kotlin/com/beust/perry/PerryModule.kt
@@ -18,19 +18,21 @@ class PerryModule : Module {
             if (isProduction) HerokuVars()
             else DevVars()
         val typedProperties = TypedProperties(vars.map)
-        binder.bind(TypedProperties::class.java).toInstance(typedProperties)
 
-        if (isProduction) {
-            binder.bind(TwitterService::class.java).to(RealTwitterService::class.java)
-            binder.bind(EmailSender::class.java).to(ProductionEmailSender::class.java)
-            binder.bind<String>(String::class.java).annotatedWith(Host::class.java).toInstance(Urls.HOST)
-        } else {
-            binder.bind(TwitterService::class.java).to(FakeTwitterService::class.java)
-            binder.bind(EmailSender::class.java).to(FakeEmailSender::class.java)
-            binder.bind(String::class.java).annotatedWith(Host::class.java).toInstance("http://localhost:9000")
+        with(binder) {
+            bind(TypedProperties::class.java).toInstance(typedProperties)
+
+            if (isProduction) {
+                bind(TwitterService::class.java).to(RealTwitterService::class.java)
+                bind(EmailSender::class.java).to(ProductionEmailSender::class.java)
+                bind<String>(String::class.java).annotatedWith(Host::class.java).toInstance(Urls.HOST)
+            } else {
+                bind(TwitterService::class.java).to(FakeTwitterService::class.java)
+                bind(EmailSender::class.java).to(FakeEmailSender::class.java)
+                bind(String::class.java).annotatedWith(Host::class.java).toInstance("http://localhost:9000")
+            }
+            bind(MetricRegistry::class.java).toInstance(MetricRegistry())
+            bind(CoverCacheMetric::class.java).toInstance(CoverCacheMetric(LocalDateTime.now()))
         }
-
-        binder.bind(MetricRegistry::class.java).toInstance(MetricRegistry())
-        binder.bind(CoverCacheMetric::class.java).toInstance(CoverCacheMetric(LocalDateTime.now()))
     }
 }
