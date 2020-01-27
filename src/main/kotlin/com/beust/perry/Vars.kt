bf926942b2e607db49816e0f7b3d82459feeaa06
Cedric Beust <cedric@beust.com>
2020-01-27 11:51:46 -0800

    Refactor Vars.kt

--- src/main/kotlin/com/beust/perry/Vars.kt
+++ src/main/kotlin/com/beust/perry/Vars.kt
@@ -1,11 +1,16 @@
 package com.beust.perry
 
-import org.slf4j.LoggerFactory
-
 open class Vars {
     val map = hashMapOf<String, String>()
+
+    fun add(property: LocalProperty, value: String) {
+        map[property.toString()] = value
+    }
 }
 
+/**
+ * Development variables are all read from local.properties.
+ */
 class DevVars: Vars() {
     init {
         val lp = LocalProperties()
@@ -13,35 +18,33 @@ class DevVars: Vars() {
             map[it] = lp.get(it)
         }
     }
-
 }
-class HerokuVars: Vars() {
-    private val log = LoggerFactory.getLogger(HerokuVars::class.java)
 
+open class EnvVars(vararg properties: LocalProperty): Vars() {
     init {
-        map[LocalProperty.JDBC_USERNAME.toString()] = System.getenv("JDBC_DATABASE_USERNAME")
-        map[LocalProperty.JDBC_PASSWORD.toString()] = System.getenv("JDBC_DATABASE_PASSWORD")
-        map[LocalProperty.JDBC_URL.toString()] = System.getenv("JDBC_DATABASE_URL")
-
-        // Extract username and password from DATABASE_URL
-//        val dbUrl = System.getenv("DATABASE_URL")
-//        URI(dbUrl).let { dbUri ->
-//            dbUri.userInfo.split(":").let { split ->
-//                val username = split[0]
-//                val password = split[1]
-//                map[LocalProperty.JDBC_USERNAME.toString()] = username
-//                map[LocalProperty.JDBC_PASSWORD.toString()] = password
-//                map[LocalProperty.JDBC_URL.toString()] = System.getenv("JDBC_DATABASE_URL")
-//            }
-//        }
-
-        map[LocalProperty.DATABASE.toString()] = "postgresql"
-        map[LocalProperty.HOST.toString()] = Urls.HOST
-
-        listOf(LocalProperty.EMAIL_USERNAME, LocalProperty.EMAIL_PASSWORD, LocalProperty.TWITTER_ACCESS_TOKEN,
-                LocalProperty.TWITTER_ACCESS_TOKEN_SECRET, LocalProperty.TWITTER_CONSUMER_KEY,
-                LocalProperty.TWITTER_CONSUMER_KEY_SECRET).map { it.toString() }.forEach {
+        properties.map { it.toString() }.forEach {
             map[it] = System.getenv(it)
         }
+    }
+
+    fun addEnv(property: LocalProperty, envName: String) {
+        map[property.toString()] = System.getenv(envName)!!
+    }
+}
+
+/**
+ * Heroku variables all come from the environment, with a few exceptions.
+ */
+class HerokuVars: EnvVars(LocalProperty.EMAIL_USERNAME, LocalProperty.EMAIL_PASSWORD,
+        LocalProperty.TWITTER_ACCESS_TOKEN, LocalProperty.TWITTER_ACCESS_TOKEN_SECRET,
+        LocalProperty.TWITTER_CONSUMER_KEY, LocalProperty.TWITTER_CONSUMER_KEY_SECRET)
+{
+    init {
+        addEnv(LocalProperty.JDBC_USERNAME, "JDBC_DATABASE_USERNAME")
+        addEnv(LocalProperty.JDBC_PASSWORD, "JDBC_DATABASE_PASSWORD")
+        addEnv(LocalProperty.JDBC_URL, "JDBC_DATABASE_URL")
+
+        add(LocalProperty.DATABASE, "postgresql")
+        add(LocalProperty.HOST, Urls.HOST)
     }
 }
